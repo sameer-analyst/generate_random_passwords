742534e24800c66668c0a469cd1d6a618cef8f6e
sameer-analyst <sameerkhan25012004@gmail.com>
2025-08-30 21:54:54 +0530

    Update generate_random_password.py

--- generate_random_password.py
+++ generate_random_password.py
@@ -1,5 +1,6 @@
 import string
 import random
 n = int(input("Enter the number of character you want in your password:"))
-generate_password = "".join(random.choices(string.ascii_letters+string.digits,k=n))
-print(generate_password)
+all_characters = string.ascii_letters + string.digits + string.punctuation
+generate_password = "".join(random.choices(all_characters,k=n))
+print("Your secure password is:",generate_password)
